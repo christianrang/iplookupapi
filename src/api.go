96814e5b5d1f767bdd0ed92455fc99df2658adc3
Christian Rang <christi1427@gmail.com>
2022-01-29 07:26:49 -0500

    added testing for api.go and reduced repeatitive lines of code

--- src/api.go
+++ src/api.go
@@ -17,15 +17,28 @@ var vtUrl = "https://www.virustotal.com/vtapi/v2"
 type IPResponse struct {
 	Ip         string                 `json:"ip"`
 	Virustotal map[string]interface{} `json:"virustotal"`
+	IPInfo     map[string]interface{} `json:"ipinfo"`
 }
 
-func SearchIP(context *gin.Context) {
-	ipResp := IPResponse{
-		Ip: context.Params.ByName("ip"),
-	}
+// type DomainResponse struct {
+// 	Domain     string `json:"domain"`
+// 	Virustotal string `json:virustotal`
+// }
 
-	url := fmt.Sprintf("%s/ip-address/report?apikey=%s&ip=%s", vtUrl, vtApiKey, ipResp.Ip)
+// type IPInfo struct {
+// 	Ip       string `json:"ip"`
+// 	Hostname string `json:"hostname"`
+// 	City     string `json:"city"`
+// 	Region   string `json:"region"`
+// 	Country  string `json:"country"`
+// 	Loc      string `json:"loc"`
+// 	Org      string `json:"org"`
+// 	Postal   string `json:"postal"`
+// 	Timezone string `json:"timezone"`
+// 	Readme   string `json:"readme"`
+// }
 
+func APICall(url string) (*http.Response, []byte) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Println(err)
@@ -45,13 +58,36 @@ func SearchIP(context *gin.Context) {
 		log.Println(err)
 	}
 
+	return res, body
+}
+
+func (ipResp *IPResponse) VTAPICall(url string) *http.Response {
+	res, body := APICall(url)
+
 	if err := json.Unmarshal(body, &ipResp.Virustotal); err != nil {
 		log.Println(err)
 	}
 
-	out, err := json.Marshal(ipResp)
+	return res
+}
 
-	context.JSON(http.StatusOK, gin.H{
-		"message": string(out),
-	})
+func (ipResp *IPResponse) IPInfoAPICall(url string) *http.Response {
+	res, body := APICall(url)
+
+	if err := json.Unmarshal(body, &ipResp.IPInfo); err != nil {
+		log.Println(err)
+	}
+
+	return res
+}
+
+func SearchIP(context *gin.Context) {
+	ipResp := IPResponse{
+		Ip: context.Params.ByName("ip"),
+	}
+
+	ipResp.VTAPICall(fmt.Sprintf("%s/ip-address/report?apikey=%s&ip=%s", vtUrl, vtApiKey, ipResp.Ip))
+	ipResp.IPInfoAPICall(fmt.Sprintf("%s%s", "https://ipinfo.io/", ipResp.Ip))
+
+	context.JSON(http.StatusOK, ipResp)
 }
--- src/api_test.go
+++ src/api_test.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVTAPICall(t *testing.T) {
+	ipResp := IPResponse{
+		Ip: "8.8.8.8",
+	}
+
+	res := ipResp.VTAPICall(fmt.Sprintf("%s/ip-address/report?apikey=%s&ip=%s", vtUrl, vtApiKey, ipResp.Ip))
+	assert.Equal(t, 200, res.StatusCode)
+}
+
+func TestIPInfoApiCall(t *testing.T) {
+	ipResp := IPResponse{
+		Ip: "8.8.8.8",
+	}
+
+	res := ipResp.IPInfoAPICall(fmt.Sprintf("%s%s", "https://ipinfo.io/", ipResp.Ip))
+	assert.Equal(t, 200, res.StatusCode)
+}
